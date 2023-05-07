4d87d457ef989b415ae4885f1886f166b63a1dd7
Elavarasi Perumal <elavarasimadurai@gmail.com>
2023-05-07 12:16:20 +0530

    testing 1st time

---
+++ testinggit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author A711351
+ *
+ */
+module testinggit {
+}
---
+++ testinggit/src/testinggit/gitcheck.java
@@ -0,0 +1,14 @@
+package testinggit;
+
+public class gitcheck {
+
+	public gitcheck() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Checking the git....");
+
+	}
+
+}
